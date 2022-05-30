a0d9e6518eb1623b214918de82049c5f5f191563
mathurma <mathurma@calpoly.edu>
2022-05-29 21:28:37 -0700

    process functionality

---
+++ similarities.py
@@ -0,0 +1,33 @@
+"""
+This module contains functions that assess the similarity of two terms.
+
+All functions accept a term and a target to match that term with/against.
+All functions return a score representing how similar the term is to the target.
+    Identical terms have a score of 1.
+    Dissimilar terms tend to 0.
+
+Functions:
+
+    lenient_match(str, str) -> float
+    strict_match(str, str) -> float
+    spacy_sim(str, str) -> float
+"""
+# all methods of similiarityies return a value from 0 - 1 where 0 is different and 1 is similar
+
+def lenient_match(term, target):
+    from difflib import SequenceMatcher
+    sm = SequenceMatcher(term, target)
+    return sm.ratio()
+
+def strict_match(term, target):
+    if term.lower() == target.lower():
+        return 1
+    else:
+        return 0
+
+def spacy_sim(term, target):
+    import spacy
+    nlp = spacy.load("en_core_web_sm")
+    toks = nlp(term + " " + target)
+    term, target = toks[0], toks[1]
+    return target.similarity(term)
---
+++ triplets.py
@@ -0,0 +1,62 @@
+"""
+This module contains functions that extract potential triplet terms from a question.
+
+All functions accept a question as input.
+    Each question is assumed to be a single sentece only (according to spacy).
+All functions return a list containing terms from the input that likely belong in a triple.
+    This means that each term in the list could be either a subject, predicate, or object.
+    List length is not guaranteed.
+
+Functions:
+
+    named_ents_and_pos(str) -> list[str]
+    enhancedSVO(str) -> list[str]
+    nltk_pos(str) -> list[str]
+"""
+
+def named_ents_and_pos(question):
+
+    import spacy
+    nlp = spacy.load("en_core_web_sm")
+
+    triplets = set()
+
+    # Process text
+    doc = nlp(question)
+
+    # Isolate sentence
+    sentences = doc.sents
+    sentences = list(sentences) # convert generator to list
+    sentence = sentences[0]
+
+    # Extract named entities
+    for ent in doc.ents:
+        triplets.add(ent.text)
+
+    # Extract non-entities
+    def _tree(tok, pos):
+        if tok.dep_ in pos:
+            yield tok.text
+        for tok in tok.children:
+            yield from _tree(tok, pos)
+    for term in _tree(sentence.root, ['nsubj']):
+        triplets.add(term)
+
+    return list(triplets)
+
+def enhancedSVO(question):
+    from subject_verb_object_extract import findSVOs, nlp
+    doc = nlp(question)
+    return findSVOs(doc)
+
+def nltk_pos(question):
+    import nltk
+    triplet = set()
+    pos = ["N", "V"]
+    doc = nltk.word_tokenize(question)
+    pos_tagged = nltk.pos_tag(doc)
+    for term, tag in pos_tagged:
+        for pos in pos:
+            if pos in tag:
+                triplet.add(term)
+    return list(triplet)
